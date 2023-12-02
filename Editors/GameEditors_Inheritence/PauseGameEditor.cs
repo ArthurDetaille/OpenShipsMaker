d90c4b559045c4ffc5ad34dc99b0974245dd9a11
ArthurDetaille <79215109+ArthurDetaille@users.noreply.github.com>
2023-12-02 18:37:24 +0100

    Update PauseGameEditor.cs

--- Editors/GameEditors_Inheritence/PauseGameEditor.cs
+++ Editors/GameEditors_Inheritence/PauseGameEditor.cs
@@ -1,5 +1,15 @@
 using UnityEngine;
 
 public class PauseGameEditor : GameEditor {
-       
+    public override void Call() {
+
+    }
+
+    public override void Close() {
+
+    }
+
+    public override void EditorLogic() {
+
+    }
 }
