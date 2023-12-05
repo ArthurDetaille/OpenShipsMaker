1eb219adec84fcadb3c62ee85625a9ea6a77a8db
ArthurDetaille <79215109+ArthurDetaille@users.noreply.github.com>
2023-12-05 14:26:17 +0100

    Scroll

--- Controllers/PivotCameraController.cs
+++ Controllers/PivotCameraController.cs
@@ -5,8 +5,12 @@ using UnityEngine;
 public class PivotCameraController : MonoBehaviour
 {
     [SerializeField]    private float sensibility;
+    [SerializeField]    private float scroll_sensibility;
     [SerializeField]    private float distance;
     [SerializeField]    private GameEditorManager editorManager;
+
+    private static float MAX_CAMERA_DISTANCE = 50.0f;
+    private static float MIN_CAMERA_DISTANCE = 5.0f;
 
     private Camera camera;
 
@@ -20,6 +24,19 @@ public class PivotCameraController : MonoBehaviour
 
     private void HandleMovement() {
         this.transform.RotateAround(Vector3.up, Input.GetAxis("look_right") * sensibility * Time.deltaTime);  
-        this.transform.RotateAround(camera.transform.right, -Input.GetAxis("look_up") * sensibility * Time.deltaTime);   
+        this.transform.RotateAround(camera.transform.right, -Input.GetAxis("look_up") * sensibility * Time.deltaTime);
+
+
+        float scroll_delta = Input.GetAxis("scroll_up") * scroll_sensibility * Time.deltaTime;
+
+        if (Mathf.Abs(scroll_delta) >= 0.01f) {
+            float raw_new_distance = distance - scroll_delta;
+            float new_distance = Mathf.Clamp(raw_new_distance, MIN_CAMERA_DISTANCE, MAX_CAMERA_DISTANCE);
+            
+            LeanTween.value(camera.gameObject, distance, new_distance, 0.1f).setOnUpdate((t) => {
+                distance = t;
+                camera.transform.localPosition = -Vector3.forward * distance;
+            });
+        }
     }
 }
